1da9a7996143c58ada68a2723a24caafde413225
Rakshitha H S <rakshithahv003@gmail.com>
2025-05-06 09:31:56 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -4,6 +4,11 @@ public class Program1 {
 
 	public static void main(String[] args) {
 System.out.println("Hello World!");
+		Scanner sc=new Scanner(System.in);
+		System.out .print("Enter a number:);
+		int n=sc.nextInt();
+		System.out.println("square of "+ n +" is: "+(n*n));
+			
 	}
 
 }
